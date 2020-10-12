a0fe8d46e25ead87f06291439db655e6613d3df7
xxxoo <xxxoo>
2020-10-11 22:37:04 -0700

    fix build issue

---
+++ Example/PopupDialog/PopupDialog.h
@@ -0,0 +1,19 @@
+//
+//  PopupDialog.h
+//  PopupDialog
+//
+//  Created by xuwenquan on 2020/10/11.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopupDialog.
+FOUNDATION_EXPORT double PopupDialogVersionNumber;
+
+//! Project version string for PopupDialog.
+FOUNDATION_EXPORT const unsigned char PopupDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupDialog/PublicHeader.h>
+
+
