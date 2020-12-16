c31762d77f8c0ef55b35d56e22ab68d714c0229f
xxxoo <xxxoo>
2020-12-15 22:08:01 -0800

    could publish

---
+++ Example/libpopupdialog/libpopupdialog.h
@@ -0,0 +1,13 @@
+//
+//  libpopupdialog.h
+//  libpopupdialog
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libpopupdialog : NSObject
+
+@end
---
+++ Example/libpopupdialog/libpopupdialog.m
@@ -0,0 +1,13 @@
+//
+//  libpopupdialog.m
+//  libpopupdialog
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+#import "libpopupdialog.h"
+
+@implementation libpopupdialog
+
+@end
